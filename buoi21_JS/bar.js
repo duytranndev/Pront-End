2821c92a2112a2fcefa6a4ed5b79726db93e4f6b
duytranndev <duytrann.dev@gmail.com>
2020-09-27 10:48:37 +0700

    master

--- buoi21_JS/bar.js
+++ buoi21_JS/bar.js
@@ -1,1 +1,12 @@
-console.log("à thế à?")
+
+
+var a=document.querySelector(".abc");
+//  console.log(a);
+
+var y=document.querySelectorAll(".btn");
+//console.log(y)
+
+for (var i = 0; i < y.length; i++) {
+    console.log(y[i])
+    y[i].innerHTML="thinh ngu";
+}
